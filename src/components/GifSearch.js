088d25f92e062ede0990f685e93c01aede3608ef
Diana Mora <morra.dianna@gmail.com>
2021-03-17 19:43:03 -0400

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,27 @@
+//The <GifSearch /> component will render a form that receives the user input for the Giphy search. The text input should be a controlled component that stores the value of the input in its component state and renders the DOM accordingly. The React component is always in charge of what the DOM looks like.
+import React, { Component } from 'react'
+
+class GifSearch extends Component {
+
+  state = {
+    query: ""
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.fetchGIFs(this.state.query)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.query} onChange={event => this.setState({query: event.target.value})} />
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default GifSearch
